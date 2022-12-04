8ff953c40e271c115c72dfa2f602cd4a0fbd69c3
osi001 <85637672+osi001@users.noreply.github.com>
2022-12-04 19:52:52 +0000

    navigation updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 type Props = {};
 
@@ -45,34 +46,35 @@ function Header({}: Props) {
         />
       </motion.div>
 
-      <motion.div
-        initial={{
-          x: 500,
-          opacity: 0,
-          scale: 0.5,
-        }}
-        animate={{
-          x: 0,
-          opacity: 1,
-          scale: 1,
-        }}
-        transition={{
-          duration: 2,
-        }}
-        className=" flex flex-row items-center text-gray-300 cursor-pointer"
-      >
-        <SocialIcon
-          className=" cursor-pointer"
-          network="email"
-          fgColor="grey"
-          bgColor="transparent"
-        />
-
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
-          {" "}
-          Connect with me
-        </p>
-      </motion.div>
+      <Link href="#contact">
+        <motion.div
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{
+            duration: 2,
+          }}
+          className=" flex flex-row items-center text-gray-300 cursor-pointer"
+        >
+          <SocialIcon
+            className=" cursor-pointer"
+            network="email"
+            fgColor="grey"
+            bgColor="transparent"
+          />
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+            {" "}
+            Connect with me
+          </p>
+        </motion.div>
+      </Link>
     </header>
   );
 }
